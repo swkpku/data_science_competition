586a3d0e21b8892d3457f424f4384c2517277b62
Ubuntu <weiso@swk-ubuntu-xiangli.nat4gcgdvrhubcisahmt5ogu1c.xx.internal.cloudapp.net>
2017-10-12 23:57:05 +0000

    add DenseNet

--- Cdiscount/src/model/densenet.py
+++ Cdiscount/src/model/densenet.py
@@ -151,6 +151,6 @@ class DenseNet(nn.Module):
     def forward(self, x):
         features = self.features(x)
         out = F.relu(features, inplace=True)
-        out = F.avg_pool2d(out, kernel_size=5, stride=2).view(features.size(0), -1)
+        out = F.avg_pool2d(out, kernel_size=3, stride=2).view(features.size(0), -1)
         out = self.classifier(out)
         return out
